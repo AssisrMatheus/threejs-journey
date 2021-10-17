c4bea16ab744b0ec60680f4294e7c93edaadf496
AssisrMatheus <assisr.matheus@gmail.com>
2021-10-16 22:46:43 -0300

    finished 11.textures

--- 11.textures/src/script.js
+++ 11.textures/src/script.js
@@ -15,9 +15,106 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+
+/**
+ * Texture(Javascript native)
+ */
+// const image = new Image()
+// const texture = new THREE.Texture(image)
+// image.addEventListener('load', () =>
+// {
+//     texture.needsUpdate = true
+// })
+// image.src = '/textures/door/color.jpg'
+
+/**
+ * Texture(Threejs loader)
+ */
+// const textureLoader = new THREE.TextureLoader()
+// const texture = textureLoader.load(
+//     '/textures/door/color.jpg',
+//     () => {
+//         console.log('loading finished')
+//     },
+//     () => {
+//         console.log('loading progressing')
+//     },
+//     () => {
+//           console.log('loading error')
+//     }
+// )
+
+// const material = new THREE.MeshBasicMaterial({ map: texture })
+
+/**
+ * Texture(Loading manager)
+ */
+const loadingManager = new THREE.LoadingManager()
+loadingManager.onStart = () => {
+    console.log('loading started')
+}
+loadingManager.onLoad = () => {
+    console.log('loading finished')
+}
+loadingManager.onProgress = () => {
+    console.log('loading progressing')
+}
+loadingManager.onError = () => {
+    console.log('loading error')
+}
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+// const colorTexture = textureLoader.load('/textures/door/color.jpg')
+/**
+ * Tiling
+ */
+// colorTexture.repeat.x = 2
+// colorTexture.repeat.y = 3
+// colorTexture.wrapS = THREE.RepeatWrapping
+// colorTexture.wrapT = THREE.RepeatWrapping
+/**
+ * Tiling(Inverted)
+ */
+// colorTexture.wrapS = THREE.MirroredRepeatWrapping
+// colorTexture.wrapT = THREE.MirroredRepeatWrapping
+
+// colorTexture.offset.x = 0.5
+// colorTexture.offset.y = 0.5
+
+// colorTexture.rotation = Math.PI * 0.25
+// colorTexture.center.x = 0.5
+// colorTexture.center.y = 0.5
+
+/**
+ * Minification filters
+ */
+// const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
+// Deactivate mipmaps if using nearest
+// colorTexture.generateMipmaps = false
+// colorTexture.minFilter = THREE.NearestFilter
+
+/**
+ * Magnification filters
+ */
+// const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
+const colorTexture = textureLoader.load('/textures/minecraft.png')
+
+colorTexture.magFilter = THREE.NearestFilter
+
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const heightTexture = textureLoader.load('/textures/door/height.jpg')
+const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+const material = new THREE.MeshBasicMaterial({ map: colorTexture })
+
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+console.log(geometry.attributes.uv)
 
 /**
  * Sizes
